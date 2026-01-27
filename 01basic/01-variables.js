ce037202f3e550ad6ebd001ce4197d6ffa4f377a
unrealaru-hub <unreal.aru@gmail.com>
2026-01-27 08:34:00 +0530

    class1st nots

---
+++ 01basic/01-variables.js
@@ -0,0 +1,99 @@
+console.log("hi 22 i am arun")
+console.log("i love java script!");
+
+age = 24;
+let name = "arun singh";
+radius = 23;
+radius = 23;
+
+console.log(age);
+console.log("age");
+console.log(radius + 1);
+
+
+const a = 10;
+console.log(a);
+
+{
+let fullName = "arun";
+console.log(fullName);
+
+ }   //its a block scope
+// console.log(fullName);  can not accessable 
+
+
+
+
+{
+var lastName = "arun rawat"
+console.log(lastName);
+
+}
+// console.log(lastName);
+
+
+
+// OBJECT:
+const student = {
+    fullname : "arun",
+    isPass : true,
+    cgps : 5.2,
+    age : 24
+
+}
+console.log(student);
+console.log(typeof(student));
+console.log(student.isPass);
+console.log(student["age"]);  //another methode to get value
+console.log(student.fullname);
+console.log(student.cgps);
+
+student["age"] = student["age"] + 10;
+console.log(student["age"]);
+
+
+
+
+
+// String
+let fullName = "arun singh rawat";
+console.log(fullName);
+console.log(typeof(fullName));
+
+
+
+// Number 
+const old = 24;
+console.log(old);
+console.log(typeof(old));
+
+
+
+// undefind 
+let value;
+console.log(value);
+console.log(typeof(value));
+
+
+
+// null 
+let cast = null;
+console.log(cast);
+console.log(typeof(cast));
+
+
+
+// bigint 
+let rollNumber = BigInt(12345);
+console.log(rollNumber);
+console.log(typeof(rollNumber));
+
+
+
+// boolean 
+let eightPlus = true;
+console.log(eightPlus);
+console.log(typeof(eightPlus));
+
+
+
