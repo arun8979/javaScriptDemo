827d878f5769a3009067c73d69f46a7cedf9b9c0
unrealaru-hub <unreal.aru@gmail.com>
2026-01-27 11:39:24 +0530

    another video nots

--- 01basic/01-variables.js
+++ 01basic/01-variables.js
@@ -93,7 +93,88 @@ console.log(typeof(rollNumber));
 // boolean 
 let eightPlus = true;
 console.log(eightPlus);
-console.log(typeof(eightPlus));
+console.log(typeof(eightPlus));           
+
+
+// operator in js
+
+// arithamatic operator we have (+,-,/,*)
+let z = 2;
+let b = 10;
+let c = z + b;
+let d = z - b;
+let e = z * b;
+let f = z / b;
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+
+//modulor operator(%,**)
+let zz = 13;
+let bb = 2;
+
+console.log(zz%bb)
+console.log(zz**bb)
+
+//unary operator(a++, b--, ++a, --b)
+let zzz = 12;
+let bbb = 8;
+
+console.log(zzz++);  //post increment
+console.log(++zzz)
+
+console.log(--bbb);  //pre decrement
+console.log(bbb)
+
+//Assignment operator (=, +=, -=, /=, *= **=, %=)
+let x = 10;
+let u = 12;
+//  console.log(x=u);
+//  console.log(x);
+
+ console.log(x+=1);
+ console.log(x-=2);
+ console.log(x**=2);
+
+//comparison operator
+let r = 10;
+let i = "10";
+ console.log(r==i);
+ console.log(r===i);
+ console.log(r>=i);
+ console.log(r!==i);
+ console.log(!r>=i);
+
+//logical operator(&&, || !)
+
+let rr = 23;
+let uu = 34;
+ 
+console.log(uu >= rr && rr !== uu);
+console.log(rr>uu && rr==uu)
+
+ console.log(rr===uu || rr<uu || rr>=uu);
+
+ console.log(!rr>=uu);
+
+
+
+ let num = prompt("type an number")
+ if(num % 5 === 0){
+    console.log(num , "yes multipler of 5")
+ }else{
+    console.log(num, "not multipler of 5")
+ }
+
+
+
+
+
+
+
+ 
+
 
 
 
